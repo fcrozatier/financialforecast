e4deb076c8175e5e4aa815963a396bd454feccc1
fcrozatier <frederic.crozatier@protonmail.com>
2021-03-11 23:41:15 +0100

    add babel

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+from babel.numbers import format_currency
+#import pandas as pd
 
 st.title("Coût réel d'un achat")
 
@@ -23,16 +24,24 @@ def generate_price_label(cat):
   }
   return switcher[cat]
 
-price = st.sidebar.number_input(generate_price_label(category))
-taux = st.sidebar.slider("Taux d'intérêt de vos investissements :", 0.0, 10.0, 4.0, 0.01)
-duree = st.sidebar.slider("Durée de l'investissement en année :", 0, 50, 15)
+price = st.sidebar.number_input(generate_price_label(category), 100)
+rate = st.sidebar.slider("Taux d'intérêt de vos investissements :", 0.0, 10.0, 4.0, 0.01)
+time = st.sidebar.slider("Durée de l'investissement en année :", 0, 50, 15)
+
+
+t = np.arange(0.0, 50, 0.01)
+s = price * np.power(1 + rate / 100, t)
+cost = price * np.power(1 + rate / 100, time)
+formatted_cost = format_currency(round(cost,2), 'EUR', locale="fr_FR")
 
 fig, ax = plt.subplots()
+ax.plot(t,s)
+ax.plot([time],[cost], 'ro')
+ax.annotate('{} €'.format(round(cost,2)), xy=(time, cost), xytext=(0, -10), textcoords="offset points", ha="center", va="top", color="red")
+ax.set(xlabel='Années', ylabel='Coût (€)',
+       title="Evolution du coût")
+ax.grid()
 
-t = np.arange(0.0, 5.0, 0.01)
-s = np.cos(2*np.pi*t)
-line, = ax.plot(t, s, lw=2)
-
-
-ax.set_ylim(-2, 2)
 st.pyplot(fig)
+
+st.write("Coût d'opportunité après {} années : **{}**".format(time, formatted_cost))
